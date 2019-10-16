d93d2644d2ff37b3d91f44a59e4de09a3e0447e6
eralpkor <eralp.ny@gmail.com>
2019-10-16 16:35:58 -0400

    edit .gitignore, added dependencies npm init

---
+++ auth/restrict.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET || 'lkajsdlkjaskldj', (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Token not valid" });
+      } else {
+        req.user = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No authorization token provided" });
+  }
+  // if (req.session && req.session.user) {
+  //   next();
+  // } else {
+  //   res.status(401).json({ message: "Not Authorized" });
+  // }
+};
