1f491b579d4c629f2d1c9c977d9f4d22d77c2edf
eralpkor <eralp.ny@gmail.com>
2019-10-16 17:27:05 -0400

    make users.db, and seeds

---
+++ data/db-config.js
@@ -0,0 +1,7 @@
+const knex = require('knex');
+
+const config - require('../knexfile.js');
+
+const dbEnvironment = process.env.DB_ENV || 'development';
+
+module.exports = knex(config[dbEnvironment])
